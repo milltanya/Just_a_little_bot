8153a534d1d57f6063aa4308f5073d1f3fd5dfb3
Tatiana Miller <36414399+milltanya@users.noreply.github.com>
2018-05-13 14:14:42 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,44 +1,60 @@
-import config
+# -*- coding: utf-8 -*-
 import telebot
-import help
-import rbc_news
-import rbc_topics
+import rbc
 
-bot = telebot.TeleBot(config.token)
+token = '570771300:AAGMX2JIFGv-2gglbJZDMj0xN0MFjTjy0Es'
+bot = telebot.TeleBot(token)
+
+
+def get_help():
+    """
+    Функция возвращает справку по боту
+    :return: string
+    """
+    return 'Привет! Ты используешь бот Just_a_little_bot, который ' \
+           'позволяет тебе получать самые актуальные новости с rbc.ru.\n' \
+           'Вот что умееет бот:\n' \
+           '/help - показать все, что может бот\n' \
+           '/new_docs <N> - показать N самых свежих новостей\n' \
+           '/new_topics <N> - показать N самых свежих тем\n' \
+           '/topic <topic_name> - показать описание темы и заголовки ' \
+           '5 самых свежих новостей в этой теме\n' \
+           '/doc <doc_title> - показать текст документа ' \
+           'с заданным заголовком\n' \
+           '/words <topic_name> - показать 5 слов, лучше всего ' \
+           'характеризующих тему\n' \
+           '/describe_doc <doc_title> - вывести статистику по документу ' \
+           '(распределение частот слов, распределение длин слов)\n' \
+           '/describe_topic <topic_name> - вывести статистику по теме ' \
+           '(количество документов в теме, средняя длина документов, ' \
+           'распределение частот слов в рамках всей темы, ' \
+           'распределение длин слов в рамках всей темы)'
+
 
 
 @bot.message_handler(commands=['help', 'start'])
 def bot_help(message):
-    s = help.get_help()
-    bot.send_message(message.chat.id, s)
-    print(s)
+    bot.send_message(message.chat.id, get_help())
 
 
 @bot.message_handler(commands=['new_docs'])
 def bot_new_docs(message):
     args = message.text.split()
-    if args[1].isdigit():
-        s = rbc_news.get_news(int(args[1]))
-        bot.send_message(message.chat.id, s)
-        print(s)
+    if args[0].isdigit():
+        bot.send_message(message.chat.id, rbc.get_news(int(args[0])))
 
 
 @bot.message_handler(commands=['new_topics'])
 def bot_new_topics(message):
-    print("new_topics")
-    print(message.text)
     args = message.text.split()
-    print(args)
-    if args[1].isdigit():
-        s = rbc_topics.get_topics(int(args[1]))
-        bot.send_message(message.chat.id, s)
-        print(s)
+    if args[0].isdigit():
+        bot.send_message(message.chat.id, rbc.get_topics(int(args[0])))
 
 
 @bot.message_handler(commands=['topic'])
 def bot_new_topics(message):
     args = message.text.split()
-    topic_information = rbc_topics.get_topic_information(" ".join(args[1:]))
+    topic_information = rbc.get_topic_information(args[0])
     if topic_information is not None:
         bot.send_message(message.chat.id, topic_information)
 
---
+++ rbc.py
@@ -0,0 +1,94 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_main_soup():
+    return get_soup('https://www.rbc.ru/story/')
+
+
+def get_soup(url):
+    return BeautifulSoup(requests.get(url).text.encode(), "lxml")
+
+
+
+def get_news(number):
+    page = get_main_soup()
+    answer = ""
+    for new in page.find('div', {'class': 'news-feed__list'}).find_all('a', {'class': 'news-feed__item chrome js-yandex-counter'})[:number]:
+        new_link = new.get('href')
+        new_title = new.find('span', {'class': 'news-feed__item__title'}).text.strip()
+        answer += new_title + "\n" + new_link + "\n\n"
+    return answer
+
+
+def get_topic_list(page):
+    return page.find('div', {'class': 'l-row js-load-container'}).find_all('div', {'class': 'item item_story js-story-item'})
+
+
+def get_item_title(item):
+    return item.find('span', {'class': 'item__title'}).text.strip()
+
+
+def get_item_link(item):
+    return item.find('a').get('href')
+
+
+def get_topic_description(topic):
+    return topic.find('span', {'class': 'item__text'}).text.strip()
+
+
+def get_topics(number):
+    answer = ""
+    for topic in get_topic_list(get_main_soup())[:number]:
+        topic_link = get_item_link(topic)
+        topic_title = get_item_title(topic)
+        answer += topic_title + "\n" + topic_link + "\n\n"
+    return answer
+
+
+def get_docs(topic_link):
+    return get_soup(topic_link).find_all('div', {'class': 'item item_story-single js-story-item'})
+
+
+def get_doc_paragraphs(doc_page):
+    return doc_page.find('div', {'class': 'article__text'}).find_all('p')
+
+
+def get_doc_text(doc_title):
+    for topic in get_topic_list(get_main_soup()):
+        topic_link = get_item_link(topic)
+        documents = get_docs(topic_link)
+        for doc in documents:
+            title = get_item_title(doc)
+            if title == doc_title:
+                break
+        if title == doc_title:
+            break
+    if title == doc_title:
+        doc_paragraphs = get_doc_paragraphs(get_soup(get_item_link(doc)))
+        answer = doc_title
+        for paragraph in doc_paragraphs:
+            answer += '\n' + paragraph.text.strip()
+        return answer
+    else:
+        return None
+
+
+def get_topic_information(topic_title):
+    for topic in get_topic_list(get_main_soup()):
+        title = get_item_title(topic)
+        if title == topic_title:
+            break
+    if title == topic_title:
+        answer = topic_title
+        topic_link = get_item_link(topic)
+        topic_description = get_topic_description(topic)
+        answer += "\n" + topic_description + "\n"
+        documents = get_docs(topic_link)
+        for doc in documents[:5]:
+            doc_title = get_item_title(doc)
+            doc_link = get_item_link(doc)
+            answer += "\n" + doc_title + "\n" + doc_link + "\n"
+        return answer
+    else:
+        return None
