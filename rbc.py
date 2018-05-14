d9ae4f421afb75e7e4a72358102072acbc4d65db
Tatiana Miller <36414399+milltanya@users.noreply.github.com>
2018-05-14 03:49:33 +0300

    Update rbc.py

--- rbc.py
+++ rbc.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from time import gmtime, strftime
 session = requests.Session()
-session.max_redirects = 100
+session.max_redirects = 100000
 
 
 def make_soup(url):
